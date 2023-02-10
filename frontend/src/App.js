4411de6c78c0985e86e02d1f6ad696209063599b
the-dark-eye <nilesh.kala@konverge.ai>
2023-02-10 15:30:20 +0530

    Added push functions

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,28 @@
 import React, { Component } from "react"
-
+import Modal from "./components/Modal";
+import axios from "axios";
+import {Table} from 'reactstrap';
 class App extends Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-        companies: []
-      };
+    state = {
+      activeItem: {
+        Exchange: "",
+        Symbol: "",
+        Shortname: "",
+        Longname: "",	
+        Sector: "",	
+        Industry: "",	
+        Currentprice: 0.0,	
+        Marketcap: 0.0,	
+        Ebitda: 0.0,	
+        Revenuegrowth: 0.0,	
+        City: "",	
+        State: "",	
+        Country: "",	
+        Fulltimeemployees: 0,
+        Longbusinesssummary: "",	
+        Weight: 0.0
+      },
+      companies: []
     };
 
     async componentDidMount() {
@@ -20,9 +37,44 @@ class App extends Component {
       }
     }
 
+    toggle = () => {
+      this.setState({ modal: !this.state.modal });
+    };
+
+    handleSubmit = item => {
+      this.toggle();
+      if (this.state.companies.some(e => e.Symbol === item.Symbol)) {
+        console.log("Item already exists in the database")
+        return;
+      }
+      axios.post('http://localhost:8000/', item)
+    };
+
+    createItem = () => {
+      const item = {
+            Exchange: "", 
+            Symbol: "",
+            Shortname: "",
+            Longname: "",	
+            Sector: "",	
+            Industry: "",	
+            Currentprice: 0.0,	
+            Marketcap: 0.0,	
+            Ebitda: 0.0,	
+            Revenuegrowth: 0.0,	
+            City: "",	
+            State: "",	
+            Country: "",	
+            Fulltimeemployees: 0,
+            Longbusinesssummary: "",	
+            Weight: 0.0
+          };
+      this.setState({ activeItem: item, modal: !this.state.modal });
+    };
+
     renderItems = () => {
       return this.state.companies.map(item => (
-        <tr>
+        <tr key={item.Symbol}>
           <td>{item.Exchange}</td>	
           <td>{item.Symbol}</td>	
           <td>{item.Shortname}</td>	
@@ -54,39 +106,44 @@ class App extends Component {
 
     render() {
       return (
-        <main className="content">
-        <div className="row">
-          <div className="col-md-6 col-sm-10 mx-auto p-0">
-            <div className="card p-3">
-              <table>
-                <thead>
-                  <tr>
-                    {/* {this.renderKeys()} */}
-                    <th>Exchange</th>	
-                    <th>Symbol</th>	
-                    <th>Shortname</th>	
-                    <th>Longname</th>	
-                    <th>Sector</th>	
-                    <th>Industry</th>	
-                    <th>Currentprice</th>	
-                    <th>Marketcap</th>	
-                    <th>Ebitda</th>	
-                    <th>Revenuegrowth</th>	
-                    <th>City</th>	
-                    <th>State</th>	
-                    <th>Country</th>	
-                    <th>Fulltimeemployees</th>	
-                    <th>Longbusinesssummary</th>	
-                    <th>Weight</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {this.renderItems()}
-                </tbody>
-              </table>
-            </div>
+        <main>
+        <h1 className="text-white text-uppercase text-center my-4">List of all Companies</h1>
+          <div className="">
+            <button onClick={this.createItem} className="btn btn-success">Add Company</button>
           </div>
-        </div>
+          <Table striped>
+            <thead>
+              <tr>
+                {/* {this.renderKeys()} */}
+                <th>Exchange</th>	
+                <th>Symbol</th>	
+                <th>Shortname</th>	
+                <th>Longname</th>	
+                <th>Sector</th>	
+                <th>Industry</th>	
+                <th>Currentprice</th>	
+                <th>Marketcap</th>	
+                <th>Ebitda</th>	
+                <th>Revenuegrowth</th>	
+                <th>City</th>	
+                <th>State</th>	
+                <th>Country</th>	
+                <th>Fulltimeemployees</th>	
+                <th>Longbusinesssummary</th>	
+                <th>Weight</th>
+              </tr>
+            </thead>
+            <tbody>
+              {this.renderItems()}
+            </tbody>
+          </Table>
+        {this.state.modal ? (
+          <Modal
+            activeItem={this.state.activeItem}
+            toggle={this.toggle}
+            onSave={this.handleSubmit}
+          />
+        ): null}
       </main>
       )
     }
---
+++ frontend/src/components/modal.js
@@ -0,0 +1,203 @@
+import React, {Component} from "react";
+import {
+    Button,
+    Modal,
+    ModalHeader,
+    ModalBody,
+    ModalFooter,
+    Form,
+    FormGroup,
+    Input,
+    Label
+} from "reactstrap";
+
+export default class CustomModal extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            activeItem: this.props.activeItem
+        };
+    }
+    handleChange = e => {
+        let {name, value} = e.target;
+        const activeItem = { ...this.state.activeItem, [name]: value };
+        this.setState({activeItem});
+    };
+    render() {
+        const {toggle, onSave} = this.props;
+        return (
+            <Modal isOpen={true} toggle={toggle}>
+                <ModalHeader toggle={toggle}>Add Company</ModalHeader>
+                <ModalBody>
+                    <Form>
+                        <FormGroup>
+                            <Label for="Exchange">Exchange</Label>
+                            <Input
+                                type="text"
+                                name="Exchange"
+                                value={this.state.activeItem.Exchange}
+                                onChange={this.handleChange}
+                                placeholder="maximum length 3"
+                            />
+                        </FormGroup>
+                        <FormGroup>
+                            <Label for="Symbol">Symbol</Label>
+                            <Input
+                                type="text"
+                                name="Symbol"
+                                value={this.state.activeItem.Symbol}
+                                onChange={this.handleChange}
+                                placeholder="maximum length 5"
+                            />
+                        </FormGroup>
+                        <FormGroup>
+                            <Label for="Shortname">Shortname</Label>
+                            <Input
+                                type="text"
+                                name="Shortname"
+                                value={this.state.activeItem.Shortname}
+                                onChange={this.handleChange}
+                                placeholder="maximum length 50"
+                            />
+                        </FormGroup>
+                        <FormGroup>
+                            <Label for="Longname">Longname</Label>
+                            <Input
+                                type="text"
+                                name="Longname"
+                                value={this.state.activeItem.Longname}
+                                onChange={this.handleChange}
+                                placeholder="maximum length 100"
+                            />
+                        </FormGroup>
+                        <FormGroup>
+                            <Label for="Sector">Sector</Label>
+                            <Input
+                                type="text"
+                                name="Sector"
+                                value={this.state.activeItem.Sector}
+                                onChange={this.handleChange}
+                                placeholder="maximum length 50"
+                            />
+                        </FormGroup>
+                        <FormGroup>
+                            <Label for="Industry">Industry</Label>
+                            <Input
+                                type="text"
+                                name="Industry"
+                                value={this.state.activeItem.Industry}
+                                onChange={this.handleChange}
+                                placeholder="maximum length 100"
+                            />
+                        </FormGroup>
+                        <FormGroup>
+                            <Label for="Currentprice">Currentprice</Label>
+                            <Input
+                                type="number"
+                                name="Currentprice"
+                                value={this.state.activeItem.Currentprice}
+                                onChange={this.handleChange}
+                                placeholder="0"
+                            />
+                        </FormGroup>
+                        <FormGroup>
+                            <Label for="Marketcap">Marketcap</Label>
+                            <Input
+                                type="number"
+                                name="Marketcap"
+                                value={this.state.activeItem.Marketcap}
+                                onChange={this.handleChange}
+                                placeholder="0"
+                            />
+                        </FormGroup>
+                        <FormGroup>
+                            <Label for="Ebitda">Ebitda</Label>
+                            <Input
+                                type="number"
+                                name="Ebitda"
+                                value={this.state.activeItem.Ebitda}
+                                onChange={this.handleChange}
+                                placeholder="maximum length 100"
+                            />
+                        </FormGroup>
+                        <FormGroup>
+                            <Label for="Revenuegrowth">Revenuegrowth</Label>
+                            <Input
+                                type="number"
+                                name="Revenuegrowth"
+                                value={this.state.activeItem.Revenuegrowth}
+                                onChange={this.handleChange}
+                                placeholder="maximum length 100"
+                            />
+                        </FormGroup>
+                        <FormGroup>
+                            <Label for="City">City</Label>
+                            <Input
+                                type="text"
+                                name="City"
+                                value={this.state.activeItem.City}
+                                onChange={this.handleChange}
+                                placeholder="maximum length 50"
+                            />
+                        </FormGroup>
+                        <FormGroup>
+                            <Label for="State">State</Label>
+                            <Input
+                                type="text"
+                                name="State"
+                                value={this.state.activeItem.State}
+                                onChange={this.handleChange}
+                                placeholder="maximum length 2"
+                            />
+                        </FormGroup>
+                        <FormGroup>
+                            <Label for="Country">Country</Label>
+                            <Input
+                                type="text"
+                                name="Country"
+                                value={this.state.activeItem.Country}
+                                onChange={this.handleChange}
+                                placeholder="maximum length 50"
+                            />
+                        </FormGroup>
+                        <FormGroup>
+                            <Label for="Fulltimeemployees">Fulltimeemployees</Label>
+                            <Input
+                                type="number"
+                                name="Fulltimeemployees"
+                                value={this.state.activeItem.Fulltimeemployees}
+                                onChange={this.handleChange}
+                                placeholder="0"
+                            />
+                        </FormGroup>
+                        <FormGroup>
+                            <Label for="Longbusinesssummary">Longbusinesssummary</Label>
+                            <Input
+                                type="text"
+                                name="Longbusinesssummary"
+                                value={this.state.activeItem.Longbusinesssummary}
+                                onChange={this.handleChange}
+                                placeholder="maximum length 5000"
+                            />
+                        </FormGroup>
+                        <FormGroup>
+                            <Label for="Weight">Weight</Label>
+                            <Input
+                                type="number"
+                                name="Weight"
+                                value={this.state.activeItem.Weight}
+                                onChange={this.handleChange}
+                                placeholder="0.0"
+                            />
+                        </FormGroup>
+                    </Form>
+                </ModalBody>
+                <ModalFooter>
+                    <Button color="success" onClick={() => onSave(this.state.activeItem)}>
+                        Save
+                    </Button>
+                </ModalFooter>
+            </Modal>
+        );
+    }
+}
